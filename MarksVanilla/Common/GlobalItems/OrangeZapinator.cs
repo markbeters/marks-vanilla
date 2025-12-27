3b2c14c725747749f1bbb5b5a162dc45aa7fd3c2
markbeters <markzpeters@outlook.com>
2025-12-27 16:49:52 -0500

    Orange Zapinator no longer receives Meteor Armour set bonus.

---
+++ MarksVanilla/Common/GlobalItems/OrangeZapinator.cs
@@ -0,0 +1,35 @@
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+
+namespace MarksVanilla.Common.GlobalItems
+{
+
+	// 
+	public class OrangeZapinatorChanges : GlobalItem
+	{
+		// Only instancing this for Orange Zapinator item
+		public override bool AppliesToEntity(Item item, bool lateInstantiation) {
+			return item.type == ItemID.ZapinatorOrange; // grab the int id for Orange Zapinator
+		}
+
+        public override bool Shoot(Item item, Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
+        {
+            if (player.setBonus == "Refunds 4 mana per attack")
+            {
+                
+
+                player.statMana -= (int)(16 * player.manaCost); //this takes the base mana cost of 16 and applies proper mana reduction to it 
+                // TODO: change so that it grabs default value instead of magic number (both above and below)
+                player.manaRegenDelay = 90;
+
+            }
+            return true; //make sure to return true, we only want to override Shoot when our set bonus is the meteor armour's
+
+        }
+		
+	}
+}
